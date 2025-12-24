005cbd89209ca444c84410a6929876d1a3df924e
conference-supp <zkghhg@gmail.com>
2025-12-24 13:03:10 -0500

    create a web server for lissajous gif

---
+++ ch1/server4/main.go
@@ -0,0 +1,41 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 20.
+//!+
+
+// Server2 is a minimal "echo" and counter server.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"gopl.io/ch1/lissajous/lissajous"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
+		count++
+		mu.Unlock()
+		lissajous.Lissajous(w)
+	}
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// counter echoes the number of calls so far.
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+//!-
